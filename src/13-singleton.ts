8c696aa031b0eb175e5a5722f4dbfd123405f342
Nicolas Molina Monroy <hi@nicobytes.com>
2022-02-09 15:47:16 -0400

    :construction: Apply singleton

---
+++ src/13-singleton.ts
@@ -0,0 +1,27 @@
+export class MyService {
+
+  static instance: MyService | null = null;
+
+  private constructor(private name: string) {}
+
+  getName() {
+    return this.name;
+  }
+
+  static create(name: string) {
+    if (MyService.instance === null) {
+      console.log('debería entrar una vez');
+      MyService.instance = new MyService(name);
+    }
+    return MyService.instance;
+  }
+}
+
+const myService1 = MyService.create('service 1');
+console.log(myService1.getName());
+const myService2 = MyService.create('service 2');
+console.log(myService2.getName());
+const myService3 = MyService.create('service 3');
+console.log(myService3.getName());
+
+console.log(myService1 === myService2);
